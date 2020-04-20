cb6808714e989a8c8b0b709f53720f987b12aae4
8-codes <master@8-codes.com>
2020-04-20 20:22:57 +0900

    first commit

---
+++ src/main/java/com/example/notice/KafkaListener.java
@@ -0,0 +1,68 @@
+package com.example.notice;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
+
+
+@Service
+public class KafkaListener {
+
+    @StreamListener(Processor.INPUT)
+    public void onReservationReservedEvent(@Payload ReservationReserved reservationReserved) {
+        if(reservationReserved.getEventType().equals("ReservationReserved")) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = null;
+            try {
+                json = objectMapper.writeValueAsString(new SmsMessage(reservationReserved.getPhoneNumber(),"예약 되었습니다."));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("JSON format exception", e);
+            }
+
+            Processor processor = NoticeApplication.applicationContext.getBean(Processor.class);
+            MessageChannel outputChannel = processor.output();
+            outputChannel.send(MessageBuilder.withPayload(json).setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON).build());
+        }
+    }
+
+    @StreamListener(Processor.INPUT)
+    public void onReservationChangedEvent(@Payload ReservationChanged reservationChanged) {
+        if(reservationChanged.getEventType().equals("ReservationChanged")) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = null;
+            try {
+                json = objectMapper.writeValueAsString(new SmsMessage(reservationChanged.getPhoneNumber(),"예약 변경 되었습니다.."));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("JSON format exception", e);
+            }
+
+            Processor processor = NoticeApplication.applicationContext.getBean(Processor.class);
+            MessageChannel outputChannel = processor.output();
+            outputChannel.send(MessageBuilder.withPayload(json).setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON).build());
+        }
+    }
+
+    @StreamListener(Processor.INPUT)
+    public void onReservationCanceledEvent(@Payload ReservationCanceled reservationCanceled) {
+        if(reservationCanceled.getEventType().equals("ReservationCanceled")) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = null;
+            try {
+                json = objectMapper.writeValueAsString(new SmsMessage(reservationCanceled.getPhoneNumber(),"예약 취소 되었습니다.."));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("JSON format exception", e);
+            }
+
+            Processor processor = NoticeApplication.applicationContext.getBean(Processor.class);
+            MessageChannel outputChannel = processor.output();
+            outputChannel.send(MessageBuilder.withPayload(json).setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON).build());
+        }
+    }
+}
---
+++ src/main/java/com/example/notice/ReservationCanceled.java
@@ -0,0 +1,56 @@
+package com.example.notice;
+
+public class ReservationCanceled {
+    private Long id;
+
+    private String eventType;
+
+
+    private String reservatorName;
+
+    private String reservationDate;
+
+    private String phoneNumber;
+
+
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReservatorName() {
+        return reservatorName;
+    }
+
+    public void setReservatorName(String reservatorName) {
+        this.reservatorName = reservatorName;
+    }
+
+    public String getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(String reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
